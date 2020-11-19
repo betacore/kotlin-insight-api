02f68612623cd8ee3cdd3462b66f94d3e023816e
Patrick Louis <patrick.louis@link-time.com>
2020-11-19 10:06:00 +0100

    Changed engine to cio

--- src/main/kotlin/com/coop/technologies/kotlinInsightApi/InsightHttpClient.kt
+++ src/main/kotlin/com/coop/technologies/kotlinInsightApi/InsightHttpClient.kt
@@ -1,14 +1,17 @@
 package com.coop.technologies.kotlinInsightApi
 
 import io.ktor.client.HttpClient
+import io.ktor.client.engine.cio.*
 import io.ktor.client.features.auth.Auth
 import io.ktor.client.features.auth.providers.basic
 import io.ktor.client.features.json.GsonSerializer
 import io.ktor.client.features.json.JsonFeature
+import io.ktor.util.*
 
 
+@KtorExperimentalAPI
 fun httpClient(user: String, pass: String) =
-    HttpClient() {
+    HttpClient(CIO) {
         install(JsonFeature) {
             serializer = GsonSerializer()
         }
